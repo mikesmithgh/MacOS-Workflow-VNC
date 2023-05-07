fff315854057d13150a97a22d56cedb7836700fa
Mike Smith <10135646+mikesmithgh@users.noreply.github.com>
2023-05-06 21:01:26 -0400

    chore: keep trying

--- configure.sh
+++ configure.sh
@@ -31,6 +31,7 @@ sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resourc
 
 #install ngrok
 brew install --cask ngrok
+brew install --cask iterm
 
 #configure ngrok and start it
 ngrok authtoken $3
